cd47d6a068fe19e5e97b6138b07e15ce47467e1b
omar kama <6611190011@mut.ac.th>
2025-11-21 14:21:16 +0700

    feat :change the theam color to red

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(
+          seedColor: const Color.fromARGB(255, 146, 28, 19),
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
